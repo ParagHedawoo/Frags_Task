0b29cf0c645debc33a1854c30c73daf249134654
ParagHedawoo <parag.hedawoo@gmail.com>
2017-03-28 11:43:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paraghedawoo/frags_task/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.paraghedawoo.frags_task;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    //onClick parameter set in xml
+    public void onClick(View v){
+        //redirects to the LocationActivity
+        if (isNetworkAvailable()) {
+            startActivity(new Intent(v.getContext(), LocationActivity.class));
+        }
+        else{
+            Toast.makeText(v.getContext(), "Check your Internet Connection again!", Toast.LENGTH_LONG).show();
+        }
+    }
+}
